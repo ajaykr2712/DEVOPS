bf708bbb6c14c5833454f447455b18f99e2dc3f1
Ajay kumar Pondugala <aponduga@cisco.com>
2025-05-14 21:55:41 +0530

    Added Changes

---
+++ Implementation/terraform/main.tf
@@ -0,0 +1,45 @@
+provider "aws" {
+  region = "us-west-2"
+}
+
+resource "aws_instance" "web_server" {
+  ami           = "ami-0c55b159cbfafe1f0"
+  instance_type = "t2.micro"
+  
+  tags = {
+    Name = "DevOps-WebServer"
+    Environment = "Development"
+  }
+  
+  user_data = <<-EOF
+              #!/bin/bash
+              echo "Hello, DevOps World!" > index.html
+              nohup python -m SimpleHTTPServer 80 &
+              EOF
+}
+
+resource "aws_security_group" "web_sg" {
+  name        = "web-server-sg"
+  description = "Allow HTTP and SSH traffic"
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
